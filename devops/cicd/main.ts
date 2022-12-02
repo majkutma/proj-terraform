884e919263b87307b758ddd87d819fc77ed91efe
Marek Majkut <marek.majkut@takeda.com>
2022-12-02 16:52:57 +0100

    s3 bucket creation in progress

--- devops/cicd/main.ts
+++ devops/cicd/main.ts
@@ -1,13 +1,19 @@
 // Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
-import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws/lib/provider"
+import { S3Bucket } from "@cdktf/provider-aws/lib/s3-bucket"
 
 class MyStack extends TerraformStack {
-  constructor(scope: Construct, id: string) {
+  constructor(scope: App, id: string) {
     super(scope, id);
 
-    // define resources here
+    new AwsProvider(this, 'AWS', {
+      region: 'us-east-1'
+    })
+
+    new S3Bucket(this, "s3-bucket", {
+    })
   }
 }
 
